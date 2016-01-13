0290e309fa108eabd4b82651d56b1f1f19946574
davidesner <esnerda@gmail.com>
2016-01-13 11:06:47 +0100

    CsvFileMerger - use the longest header in case column was added

--- src/main/java/keboola/adform/masterdata_extractor/filemerger/CsvFileMerger.java
+++ src/main/java/keboola/adform/masterdata_extractor/filemerger/CsvFileMerger.java
@@ -2,17 +2,30 @@
  */
 package keboola.adform.masterdata_extractor.filemerger;
 
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MappingJsonFactory;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -38,6 +51,12 @@ public class CsvFileMerger {
         int i = 0;
         FileChannel out = null;
         FileOutputStream fout = null;
+        try {
+            headerLine = getLongestHeaders(filePaths);
+
+        } catch (Exception ex) {
+            throw new MergeException(ex.getMessage());
+        }
         for (String fPath : filePaths) {
             FileInputStream fis = null;
             try {
@@ -45,9 +64,9 @@ public class CsvFileMerger {
                 //retrieve file header
                 fis = new FileInputStream(file);
                 reader = new BufferedReader(new InputStreamReader(fis));
-                headerLine = reader.readLine();
+                String currHeader = reader.readLine();
 
-                if (headerLine == null) {
+                if (currHeader == null) {
                     continue;
                 }
                 //write header from first file and retrieve filechannel 
@@ -64,7 +83,7 @@ public class CsvFileMerger {
                 FileChannel in = fis.getChannel();
                 long pos = 0;
                 //set position according to header (first run is set by writer)
-                pos = headerLine.length() + 2;//+2 because of NL character
+                pos = currHeader.length() + 2;//+2 because of NL character
 
                 for (long p = pos, l = in.size(); p < l;) {
                     p += in.transferTo(p, l - p, out);
@@ -88,5 +107,68 @@ public class CsvFileMerger {
             }
 
         }
+    }
+
+    /**
+     *
+     * @param filePaths - list of files to merge
+     * @return header line with the most columns
+     * @throws Exception
+     */
+    private static String getLongestHeaders(List<String> filePaths) throws Exception {
+        String[] headers = null;
+        String[] lastHeaders = null;
+        String headerLine = "";
+        String currFile = "";
+        BufferedReader reader;
+        FileInputStream fis;
+        try {
+            int maxHeaderSize = 0;
+            for (String fPath : filePaths) {
+                currFile = fPath;
+
+                File csvFile = new File(fPath);
+                FileReader freader = new FileReader(csvFile);
+                CSVReader csvreader = new CSVReader(freader, '\t', CSVWriter.NO_QUOTE_CHARACTER);
+                headers = csvreader.readNext();
+                if (headers != null) {
+                    if (headers.length > maxHeaderSize) {
+                        maxHeaderSize = headers.length;
+                        //get header line
+                        fis = new FileInputStream(fPath);
+                        reader = new BufferedReader(new InputStreamReader(fis));
+                        headerLine = reader.readLine();
+                        reader.close();
+                    }
+                    lastHeaders = csvreader.readNext();
+
+                } else {
+                    throw new Exception("Error reading csv file header: " + currFile);
+                }
+                csvreader.close();
+            }
+            if (maxHeaderSize == 0 || headerLine.equals("")) {
+                throw new Exception("Zero length header in csv file!");
+            }
+            return headerLine;
+
+        } catch (FileNotFoundException ex) {
+            throw new Exception("CSV file not found. " + currFile + " " + ex.getMessage());
+        } catch (IOException ex) {
+            throw new Exception("Error reading csv file: " + currFile + " " + ex.getMessage());
+        }
+    }
+
+    private static String implode(String separator, String... data) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < data.length - 1; i++) {
+            //data.length - 1 => to not add separator at the end
+            if (!data[i].matches(" *")) {//empty string are ""; " "; "  "; and so on
+                sb.append(data[i]);
+                sb.append(separator);
+            }
+        }
+        sb.append(data[data.length - 1].trim());
+        return sb.toString();
     }
 }
